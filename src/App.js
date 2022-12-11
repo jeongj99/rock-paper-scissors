09e670c7ab60ebc996eea4112c3d7f4cb54a7759
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-11 20:28:57 -0300

    Import User component to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
+import User from './components/User';
+
 import './App.css';
 
 function App() {
   return (
     <>
-      <div className='user-section'>
-        <h1>Display user choice here</h1>
-      </div>
-      <div className='game-section'>
-        <h1>Display game here</h1>
-      </div>
-      <div className='ai-section'>
-        <h1>Display ai choice here</h1>
-      </div>
+      <section className='game'>
+        <User />
+      </section>
     </>
   );
 }
