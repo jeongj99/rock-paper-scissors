c208e0904eedee5ccdd22ec18ee1d89f78ef2a60
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-11 20:14:48 -0300

    Add divs in App.js to set up the main page (game)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 function App() {
   return (
     <>
+      <div>
+        <h1>Display user choice here</h1>
+      </div>
+      <div>
+        <h1>Display game here</h1>
+      </div>
+      <div>
+        <h1>Display ai choice here</h1>
+      </div>
     </>
   );
 }
