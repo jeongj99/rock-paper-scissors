99bfe1763cedd8b679daef141b49055f057d19e3
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-21 21:22:31 -0300

    Create css for Computer component and add template stylistic

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -1,6 +1,8 @@
+import "./Computer.css"
+
 export default function Computer() {
   return (
-    <div>
+    <div className="computer-screen">
       <h1>Diplay computer screen here</h1>
     </div>
   );
