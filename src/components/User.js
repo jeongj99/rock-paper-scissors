08e1ee40836476d1732d8f7d12b17afb28c60d43
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-21 21:35:08 -0300

    Add Button components to User component

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,16 @@
+import Button from "./Button";
+
 import "./User.css";
 
 export default function User() {
   return (
     <div className="user-screen">
       <h1>Display user screen here</h1>
+      <div className="user-option">
+        <Button rock>Rock</Button>
+        <Button scissors>Scissors</Button>
+        <Button paper>Paper</Button>
+      </div>
     </div>
   );
 }
