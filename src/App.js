c155fd59afeb9e77a068bec16233fd218b90a51b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-24 23:48:53 -0300

    Create states and pass them as props to corresponding components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
+
 import User from './components/User';
 import Game from './components//Game';
 
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [userChoice, setUserChoice] = useState('rock');
+  const [computerChoice, setComputerChoice] = useState('scissors');
+
   return (
     <>
       <section className='game'>
-        <Game />
-        <User />
+        <Game userChoice={userChoice} computerChoice={computerChoice} />
+        <User setUserChoice={setUserChoice} setComputerChoice={setComputerChoice} />
       </section>
     </>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,15 @@
 import "./Game.css"
 
-export default function Game() {
+export default function Game(props) {
   return (
     <div className="game-screen">
       <h1>Rock Paper Scissors</h1>
       <div className="choices">
         <div className="user-choice">
-          <h1>rock</h1>
+          <h1>{props.userChoice}</h1>
         </div>
         <div className="computer-choice">
-          <h1>scissors</h1>
+          <h1>{props.computerChoice}</h1>
         </div>
       </div>
     </div>
