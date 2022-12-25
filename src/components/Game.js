c5bb4004609c0e7e39b7b93058b62acef1573ae9
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-24 23:23:11 -0300

    Add divs and style it for the Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,15 @@ import "./Game.css"
 export default function Game() {
   return (
     <div className="game-screen">
-      <h1>Display game action here</h1>
+      <h1>Rock Paper Scissors</h1>
+      <div className="choices">
+        <div className="user-choice">
+          <h1>rock</h1>
+        </div>
+        <div className="computer-choice">
+          <h1>scissors</h1>
+        </div>
+      </div>
     </div>
   )
 }
