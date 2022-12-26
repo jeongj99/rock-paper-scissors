a9f10b72831d3a74537fd3659b730b0b92b45e6d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-26 17:20:46 -0300

    Create a user and computer score state and have it displayed in the Games component

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import React from 'react';
 function App() {
   const [userChoice, setUserChoice] = useState("");
   const [computerChoice, setComputerChoice] = useState('scissors');
+  const [userScore, setUserScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
 
   return (
     <>
       <section className='game'>
-        <Game userChoice={userChoice} computerChoice={computerChoice} />
+        <Game userChoice={userChoice} computerChoice={computerChoice} userScore={userScore} computerScore={computerScore} />
         <User setUserChoice={setUserChoice} setComputerChoice={setComputerChoice} />
       </section>
     </>
--- src/components/Game.js
+++ src/components/Game.js
@@ -7,39 +7,45 @@ export default function Game(props) {
     <div className="game-screen">
       <h1>Rock Paper Scissors</h1>
       <div className="choices">
-        <div className="user-choice">
-          {props.userChoice === "rock" &&
-            <h1>
-              <FaHandRock />
-            </h1>
-          }
-          {props.userChoice === "paper" &&
-            <h1>
-              <FaHandPaper />
-            </h1>
-          }
-          {props.userChoice === "scissors" &&
-            <h1>
-              <FaHandScissors />
-            </h1>
-          }
+        <div className="user">
+          <h1>User Score: {props.userScore}</h1>
+          <div className="user-choice">
+            {props.userChoice === "rock" &&
+              <h1>
+                <FaHandRock />
+              </h1>
+            }
+            {props.userChoice === "paper" &&
+              <h1>
+                <FaHandPaper />
+              </h1>
+            }
+            {props.userChoice === "scissors" &&
+              <h1>
+                <FaHandScissors />
+              </h1>
+            }
+          </div>
         </div>
-        <div className="computer-choice">
-          {props.computerChoice === "rock" &&
-            <h1>
-              <FaHandRock />
-            </h1>
-          }
-          {props.computerChoice === "paper" &&
-            <h1>
-              <FaHandPaper />
-            </h1>
-          }
-          {props.computerChoice === "scissors" &&
-            <h1>
-              <FaHandScissors />
-            </h1>
-          }
+        <div className="computer">
+          <h1>Computer Score: {props.computerScore}</h1>
+          <div className="computer-choice">
+            {props.computerChoice === "rock" &&
+              <h1>
+                <FaHandRock />
+              </h1>
+            }
+            {props.computerChoice === "paper" &&
+              <h1>
+                <FaHandPaper />
+              </h1>
+            }
+            {props.computerChoice === "scissors" &&
+              <h1>
+                <FaHandScissors />
+              </h1>
+            }
+          </div>
         </div>
       </div>
     </div>
