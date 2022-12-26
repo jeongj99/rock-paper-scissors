92deee91f9e5770245ce4470db8c6efe7b934810
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-12-26 16:14:38 -0300

    Add conditional rendering depending on state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,6 @@
-import "./Game.css"
+import { FaHandRock, FaHandPaper, FaHandScissors } from "react-icons/fa";
+
+import "./Game.css";
 
 export default function Game(props) {
   return (
@@ -6,12 +8,26 @@ export default function Game(props) {
       <h1>Rock Paper Scissors</h1>
       <div className="choices">
         <div className="user-choice">
-          <h1>{props.userChoice}</h1>
+          {props.userChoice === "rock" &&
+            <h1>
+              <FaHandRock />
+            </h1>
+          }
+          {props.userChoice === "paper" &&
+            <h1>
+              <FaHandPaper />
+            </h1>
+          }
+          {props.userChoice === "scissors" &&
+            <h1>
+              <FaHandScissors />
+            </h1>
+          }
         </div>
         <div className="computer-choice">
           <h1>{props.computerChoice}</h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
